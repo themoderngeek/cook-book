1e34179cd8a50f7e0dab5c457e131fa6d7a6c1d7
themoderngeek <moderngeekish@gmail.com>
2015-08-06 11:27:20 +0100

    Inital project structure from spring boot

---
+++ src/main/java/uk/co/themoderngeek/cookbook/CookBookApplication.java
@@ -0,0 +1,12 @@
+package uk.co.themoderngeek.cookbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookBookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CookBookApplication.class, args);
+    }
+}
---
+++ src/test/java/uk/co/themoderngeek/cookbook/CookBookApplicationTests.java
@@ -0,0 +1,16 @@
+package uk.co.themoderngeek.cookbook;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = CookBookApplication.class)
+public class CookBookApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
